0d63658ce956b68225792f1f4bd827217c995446
jaocodes <jhonskaz@gmail.com>
2025-11-12 19:11:04 -0300

    creating Author class

---
+++ src/Author.java
@@ -0,0 +1,34 @@
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class Author {
+    private final String id;
+    private String name;
+    private LocalDate dateOfBirth;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public Author (String name, LocalDate dateOfBirth) throws IllegalArgumentException {
+
+        if(name == null || name.trim().isEmpty()) throw new IllegalArgumentException("Author name cannot be empty");
+        if(dateOfBirth == null || dateOfBirth.isAfter(LocalDate.now())) throw new IllegalArgumentException("Author date of birth cannot be null or in a future date");
+
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String toString(){
+        return String.format("ID: %s Name: %s DateOfBirth: %td/%<tm/%<tY",id,name,dateOfBirth);
+    }
+}
