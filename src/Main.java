2a327eeb9557049ac88b9cd300e14f47749b043f
jaocodes <jhonskaz@gmail.com>
2025-11-13 21:17:35 -0300

    refine main loop of library system

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,68 @@
+import javax.swing.*;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Main {
+    private static Library library = new Library();
+    private static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Library library = new Library();
-        Scanner scan = new Scanner(System.in);
 
         while (true){
             System.out.println("Deseja ver a Lista de livros? (S/N)");
-            String answer = scan.nextLine();
+            String answer = scan.nextLine().trim();
 
             if (answer.equalsIgnoreCase("s")) {
-                System.out.println("\n ----  LIVROS  ----");
-                List<Book> books = library.getBooks();
-
-                if (books.isEmpty()){
-                    System.out.println("\n NENHUM LIVRO CADASTRADO");
-                }
-                books.forEach(book -> {
-                    String printString = String.format("ID: %s - Título: %s - Autor: %s",book.getId(), book.getTitle(), book.getAuthor());
-                    System.out.println(printString);
-                });
+                showAvailableBooks();
+                processLoan();
             } else if (answer.equalsIgnoreCase("N")) {
+                System.out.println("Sistema de emprestimos será encerrado");
+
                 break; // Sai do loop
             }
         }
+        scan.close();
+    }
+    private static void showAvailableBooks(){
+        System.out.println("\n ----  LIVROS  ----");
+        List<Book> books = library.getBooks();
+
+        if (books.isEmpty()){
+            System.out.println("\n NENHUM LIVRO CADASTRADO");
+        }
+        books.forEach(book -> {
+            if(book.isAvailable()){
+                String printString = String.format("ID: %s - Título: %s - Autor: %s",book.getId(), book.getTitle(), book.getAuthor());
+                System.out.println(printString);
+            }
+        });
+    }
+
+    private static void processLoan(){
+            boolean isloopingLoan = true;
+        while (isloopingLoan){
+            System.out.println("Digite o ID do livro que deseja emprestar: ");
+            String answerId = scan.nextLine().trim();
+
+            System.out.println("Digite o seu nome para realizar o empréstimo: ");
+            String aswerName = scan.nextLine().trim();
+
+            try {
+                Loan loan = library.loanBook(answerId,aswerName);
+                String loanExecutedStr =  String.format("O livro '%s' foi emprestado para '%s' em %td de %<tb de %<tY ",loan.getBook().getTitle(),loan.getClientName(),loan.getLoanDate());
+                System.out.println(loanExecutedStr);
+                isloopingLoan =  false;
+
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+
+
 
     }
+
 }
